ee1dcf591282bbdbfbcbc3f06a5798c62b163667
Metehan Kurucu <metehankurucu@gmail.com>
2026-04-10 22:34:01 +0300

    fix: import createTracker from correct litemetrics package

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export function Analytics() {
   useEffect(() => {
-    import("@litemetrics/react").then(({ createTracker }) => {
+    import("@litemetrics/tracker").then(({ createTracker }) => {
       createTracker({
         siteId: "site_m0i7rspp7gge",
         endpoint: "https://metrics.codixus.com/api/collect",
